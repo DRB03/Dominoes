6cf0619d9b3413a19b2024db25f6690af13ca889
DRB03 <126363236+DRB03@users.noreply.github.com>
2023-03-13 19:57:55 +0000

    hola

---
+++ Dominoes/Program.cs
@@ -0,0 +1,24 @@
+﻿class Dominoes
+{
+    public int d1;
+    public int d2;
+
+    public Dominoes(int d1, int d2)
+    {this.d1=d1; this.d2=d2;}
+    public static int operator + (Dominoes a,Dominoes b)
+    {
+        int suma=a.d1 + a.d2 + b.d1 + b.d2;
+        return suma;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       Dominoes d1= new Dominoes(0,1);
+       Dominoes d2= new Dominoes(2,3);
+       int total= d1 + d2;
+       Console.WriteLine(total);
+    }
+}
